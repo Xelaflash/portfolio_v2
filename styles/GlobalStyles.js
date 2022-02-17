1b13bea66f90cafdd0a39fde46388fc015059ff7
Alexandre Gros <xelaflash@gmail.com>
2022-02-17 10:59:19 -0400

    🎨 color var change

--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -56,26 +56,36 @@ const GlobalStyle = createGlobalStyle`
 
   :root {
     --color-white: ${COLORS.white};
+    --color-green: ${COLORS.green};
+    --color-peach: ${COLORS.peach};
     --color-gray-100: ${COLORS.gray[100]};
     --color-gray-300: ${COLORS.gray[300]};
     --color-gray-500: ${COLORS.gray[500]};
     --color-gray-700: ${COLORS.gray[700]};
     --color-gray-900: ${COLORS.gray[900]};
-    --color-logo-blue: ${COLORS.logoBlue};
+    /* --color-logo-blue: ${COLORS.logoBlue};
     --color-logo-Violet: ${COLORS.logoViolet};
     --color-logo-red: ${COLORS.logoRed};
     --color-logo-orange: ${COLORS.logoOrange};
     --color-logo-yellow: ${COLORS.logoYellow};
-    --color-logo-green: ${COLORS.logoGreen};
+    --color-logo-green: ${COLORS.logoGreen}; */
 
     /* Dark Mode variables */
     @media (prefers-color-scheme: dark) {
       --color-primary: ${COLORS.darkPrimaryViolet};
       --color-secondary: ${COLORS.darkSecondaryBlue};
-      --color-tertiary: ${COLORS.darkTertiaryYellow};
       --color-background: ${COLORS.darkBackgroundBlue};
       --color-background-shade2: ${COLORS.darkBackgroundBlueShade2};
       --color-background-shade3: ${COLORS.darkBackgroundBlueShade3};
+      --color-gradient: linear-gradient(
+        90deg,
+        hsl(185deg 100% 50%) 0%,
+        hsl(196deg 100% 70%) 23%,
+        hsl(211deg 100% 77%) 33%,
+        hsl(234deg 100% 82%) 44%,
+        hsl(259deg 100% 80%) 58%,
+        hsl(274deg 100% 75%) 99%
+      );
       body {
         color: var(--color-white);
         background: var(--color-background);
@@ -84,13 +94,21 @@ const GlobalStyle = createGlobalStyle`
 
     /* Light Mode variables */
     @media (prefers-color-scheme: light) {
-      --color-primary: ${COLORS.lightPrimaryGreen};
+      --color-primary: ${COLORS.lightPrimaryBlue};
       --color-secondary: ${COLORS.lightSecondaryVioline};
-      --color-tertiary: ${COLORS.lightTertiaryLightBlue};
       --color-background: ${COLORS.white};
       --color-background-shade2: ${COLORS.lightBackgroundShade2};
       --color-background-shade3: ${COLORS.lightBackgroundShade3};
-      --color-text: ${COLORS.darkBackgroundBlue};
+      --color-text: ${COLORS.gray[900]};
+      --color-gradient: linear-gradient(
+        90deg,
+        hsl(228deg 78% 52%) 0%,
+        hsl(246deg 63% 56%) 29%,
+        hsl(263deg 54% 51%) 44%,
+        hsl(280deg 53% 46%) 57%,
+        hsl(298deg 56% 41%) 72%,
+        hsl(315deg 68% 41%) 100%
+      );
       body {
         color: var(--color-text);
         background: var(--color-background);
--- styles/constants.js
+++ styles/constants.js
@@ -11,25 +11,27 @@ export const COLORS = {
   // Dark mode colors
   darkPrimaryViolet: 'hsl(274, 100%, 75%)', // #C77DFF
   darkSecondaryBlue: 'hsl(185, 100%, 50%)', // #00E9FF
-  darkTertiaryYellow: 'hsl(53, 100%, 50%) ', // #FFE100
   darkBackgroundBlue: 'hsl(212, 32%,8%)', // #0E141B
   darkBackgroundBlueShade2: 'hsl(212, 32%,14%)', // #18232F
   darkBackgroundBlueShade3: 'hsl(212, 32%,20%)', // #233243
 
   // Light mode colors
-  lightPrimaryGreen: 'hsl(177, 40%, 45%)', // #45A29E
+  lightPrimaryBlue: 'hsl(228, 78%, 52%)', // #264DE4
   lightSecondaryVioline: 'hsl(315, 68%, 41%)', // #B0228C
-  lightTertiaryLightBlue: 'hsl(201, 55%, 75%) ', // #A8D0E6
   lightBackgroundShade2: 'hsl(180, 4%, 95%)', // #F2F3F3
   lightBackgroundShade3: 'hsl(190, 6%, 80%) ', // #C9CECF
 
+  // shared colors
+  green: 'hsl(177, 40%, 45%)', // #45A29E
+  peach: 'hsl(0, 90%, 70%)', // #F76C6C
+
   // Logo colors
-  logoBlue: 'hsl(197, 100%, 43%)', // #009DDB
-  logoViolet: 'hsl(299, 42%, 42%)', // #963D97
-  logoRed: 'hsl(359, 72%, 55%)', // #DF3A3E
-  logoOrange: 'hsl(28, 91%, 54%)', // #F5831E
-  logoYellow: 'hsl(41, 98%, 57%)', // #FDB827
-  logoGreen: 'hsl(106, 46%, 50%)', // #61BB46
+  // logoBlue: "hsl(197, 100%, 43%)", // #009DDB
+  // logoViolet: "hsl(299, 42%, 42%)", // #963D97
+  // logoRed: "hsl(359, 72%, 55%)", // #DF3A3E
+  // logoOrange: "hsl(28, 91%, 54%)", // #F5831E
+  // logoYellow: "hsl(41, 98%, 57%)", // #FDB827
+  // logoGreen: "hsl(106, 46%, 50%)", // #61BB46
 };
 
 export const WEIGHTS = {
