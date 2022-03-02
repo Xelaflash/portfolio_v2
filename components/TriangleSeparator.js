52a74071dc58032216c534ced256b14026cf3dac
AlexG <xelaflash@gmail.com>
2022-03-02 11:01:42 -0400

    📝 review separators

--- components/TriangleSeparator.js
+++ components/TriangleSeparator.js
@@ -1,34 +1,40 @@
 import styled from 'styled-components';
+import { QUERIES } from '../styles/constants';
 
 export default function TriangleSeparator() {
   return (
-    <>
-      <TriangleSeparatorWrapperTop />
-      <TriangleSeparatorWrapperBottom />
-    </>
+    <TriangleSeparatorWrapper>
+      {/* <TriangleSeparatorWrapperTop />
+      <TriangleSeparatorWrapperBottom /> */}
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 1440 301"
+        version="1.1"
+        preserveAspectRatio="xMidYMax slice"
+      >
+        <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+          <g
+            id="Desktop-HD"
+            transform="translate(0.000000, -188.000000)"
+            fill="var(--color-background-shade3)"
+            stroke="var(--color-background-shade3)"
+          >
+            <polygon
+              id="Triangle"
+              transform="translate(720.500000, 338.500000) rotate(90.000000) translate(-720.500000, -338.500000) "
+              points="720.5 -381.5 870.5 1058.5 570.5 1058.5"
+            />
+          </g>
+        </g>
+      </svg>
+    </TriangleSeparatorWrapper>
   );
 }
 
-const TriangleSeparatorWrapperTop = styled.div`
+const TriangleSeparatorWrapper = styled.div`
   margin-top: -5rem;
-  width: 100%;
-  min-height: 10rem;
-  background: linear-gradient(
-    calc(-160deg - 14deg),
-    transparent 0%,
-    transparent 49.99%,
-    var(--color-background-shade3) 50%,
-    var(--color-background-shade3) 100%
-  );
-`;
-const TriangleSeparatorWrapperBottom = styled.div`
-  width: 100%;
-  min-height: 10rem;
-  background: linear-gradient(
-    calc(160deg + 14deg),
-    var(--color-background-shade3) 0,
-    var(--color-background-shade3) 49.99%,
-    transparent 50%,
-    transparent 100%
-  );
+  @media ${QUERIES.phoneAndDown} {
+    margin-top: -3rem;
+  }
 `;
