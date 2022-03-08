b7436b73d63edc3bc861e0b6d86cb0a878b00bdf
AlexG <xelaflash@gmail.com>
2022-03-08 10:41:46 -0400

    🚀 Add type and disabled props

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,12 @@
 import propTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Button = ({ text, background, color, className }) => (
+const Button = ({ text, background, color, className, type, disabled }) => (
   <ButtonStyles
     style={{ color: `${color}`, background: `${background}` }}
     className={className}
+    type={type}
+    disabled={disabled}
   >
     {text}
   </ButtonStyles>
@@ -27,6 +29,8 @@ Button.propTypes = {
   text: propTypes.string,
   background: propTypes.string,
   color: propTypes.string,
+  type: propTypes.string,
+  disabled: propTypes.bool,
   className: propTypes.string,
 };
 
