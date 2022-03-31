c315dd6f28954f8a96d0e9aa375d09d472d568e0
AlexG <xelaflash@gmail.com>
2022-03-31 17:14:40 -0400

    🎨 animation + change display logic

--- components/ScrollToTop.js
+++ components/ScrollToTop.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { ArrowUpCircle } from 'react-feather';
 import { useState, useEffect } from 'react';
 import VisuallyHidden from './VisuallyHidden';
@@ -8,7 +8,9 @@ const ScrollToTop = () => {
   const [showTopBtn, setShowTopBtn] = useState(false);
 
   const handleScroll = () => {
-    if (window.scrollY > 400) {
+    const rootElement = document.documentElement;
+    const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+    if (rootElement.scrollTop / scrollTotal > 0.8) {
       setShowTopBtn(true);
     } else {
       setShowTopBtn(false);
@@ -28,17 +30,15 @@ const ScrollToTop = () => {
   };
 
   return (
-    <ScrollTopStyled>
-      {showTopBtn && (
-        <>
-          <ArrowUpCircle
-            className="icon-position icon-style"
-            strokeWidth={1.5}
-            onClick={goToTop}
-          />
-          <VisuallyHidden>Go To Top</VisuallyHidden>
-        </>
-      )}
+    <ScrollTopStyled isVisible={showTopBtn}>
+      <>
+        <ArrowUpCircle
+          className="icon-position icon-style"
+          strokeWidth={1.5}
+          onClick={goToTop}
+        />
+        <VisuallyHidden>Go To Top</VisuallyHidden>
+      </>
     </ScrollTopStyled>
   );
 };
@@ -53,7 +53,8 @@ const ScrollTopStyled = styled.div`
     margin-right: 20px;
     z-index: 20;
     @media ${QUERIES.phoneAndDown} {
-      right: 5px;
+      margin-right: 5px;
+      bottom: 10px;
     }
   }
   .icon-style {
@@ -63,10 +64,20 @@ const ScrollTopStyled = styled.div`
     opacity: 0.8;
     width: var(--min-tap-height);
     height: var(--min-tap-height);
-    /* width: 32px;
-    height: 32px; */
     cursor: pointer;
     transition: all 0.5s ease-in-out;
+    opacity: 0;
+    transform: translateY(100px);
+    transition: all 0.5s ease;
+    ${(props) =>
+      props.isVisible &&
+      css`
+        opacity: 1;
+        transform: translateY(0);
+      `};
+    @media ${QUERIES.phoneAndDown} {
+      right: 5px;
+    }
   }
   .icon-style:hover {
     color: var(--color-primary);
